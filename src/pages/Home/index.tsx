0fe4e7230f2f14942f27166196b4eb00dc11215a
Anael Medeiros <anael.medeiros@CTNAPPLE71P6QCM.local>
2023-04-26 18:14:03 -0400

    melhorias de layout, add rotina de mapa, add rotina de imagem

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Header } from "../../components/Header";
 import { GoogleMap, Marker, useJsApiLoader } from "@react-google-maps/api";
-
 import { MdArticle } from "react-icons/md";
 import {
   ListContainer,
